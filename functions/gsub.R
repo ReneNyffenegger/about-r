aaacd0b6f57c3b164114f4d0a179ad030a0a0f0b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-25 23:21:07 +0200

    Working on c()

--- functions/gsub.R
+++ functions/gsub.R
@@ -4,3 +4,6 @@ gsub("xyz", "bar", "foo xyz baz xyz")
 # Note the regular expression (^)
 gsub("^xyz", "bar", "foo xyz baz abcxyz")
 # [1] "foo xyz baz abcxyz"
+
+gsub("xyz", "bar", c("foo", "xyz", "baz"))
+# [1] "foo" "bar" "baz"
