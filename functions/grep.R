d3b2b424be76a7d9d63af68622320ead25aee5f4
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-25 23:31:10 +0200

    value=TRUE/FALSE

--- functions/grep.R
+++ functions/grep.R
@@ -1,4 +1,10 @@
 words=c("Foo", "Bar", "Baz", "One", "Two", "Three")
 
-grep("Ba", words)
+# grep returns a Vector of the indices of the matched elements (value=FALSE), or
+# of the matched (value=TRUE)
+
+grep("Ba", words, value=FALSE)
 # [1] 2 3
+
+grep("Ba", words, value=TRUE)
+# [1] "Bar" "Baz"
