ed873cb8a037f059621febfc8d88ece3e9456251
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 18:13:13 +0200

    -> options

--- functions/getOption.R
+++ functions/getOption.R
@@ -1,3 +1,8 @@
+#
+#  See also opions()
+#
 getOption("repos")
 #                                 CRAN                            CRANextra
 #                             "@CRAN@" "http://www.stats.ox.ac.uk/pub/RWin"
+
+getOption("width")   # width of console
