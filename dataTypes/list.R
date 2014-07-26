bef92c0655505b007b62985a549383cf1eb4b3e6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 13:23:36 +0200

    indexing

--- dataTypes/list.R
+++ dataTypes/list.R
@@ -1,3 +1,15 @@
 # Lists a.k.a. Vectors
 
-l <- c(1, 2, 3, 4)
+l <- c('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten')
+
+l[3]
+# [1] "three"
+
+l[3:5]
+# [1] "three" "four"  "five"
+
+l[11]
+# [1] NA
+
+l[0]
+# character(0)
