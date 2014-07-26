10cdb8d3f021b5dcef9bfd7ac92b777e24043651
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 14:41:22 +0200

    + data.frame

---
+++ functions/data.frame.R
@@ -0,0 +1,14 @@
+alphabeth <- c("Alpha", "Beta", "Gamma")
+numbers   <- c( 1.1   ,  2.02 ,  3.003 )
+blabla    <- c("Foo"  , "Bar" , "Baz"  )
+
+dataFrame <- data.frame(alphabeth, numbers, blabla)
+
+show (dataFrame)
+#   alphabeth numbers blabla
+# 1     Alpha   1.100    Foo
+# 2      Beta   2.020    Bar
+# 3     Gamma   3.003    Baz
+
+
+
