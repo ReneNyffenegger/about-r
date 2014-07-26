3ae02d76c92f0ee1a5c2603144324a97ef8b9aa4
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 13:27:16 +0200

    + factors

---
+++ dataTypes/factors.R
@@ -0,0 +1,9 @@
+vec <- c(1:10)
+summary (vec)
+#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+#  1.00    3.25    5.50    5.50    7.75   10.00
+
+fac <- factor(vec)
+summary (fac)
+#  1  2  3  4  5  6  7  8  9 10
+#  1  1  1  1  1  1  1  1  1  1
