a7e8142000df3aeb9111eacbb8ed5bb6043460c3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 13:37:38 +0200

    more summary

--- functions/summary.R
+++ functions/summary.R
@@ -1,7 +1,14 @@
+summary(c(  4, 9, 5, 1000, 11, 5, 8) )
+#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+#   4.0     5.0     8.0   148.9    10.0  1000.0
+
+summary("Foo")
+#  Length     Class      Mode
+#       1 character character
+
 # Summary of a Factor:
 #
 #   print Elements along with their frequency:
-
 summary (factor (c ( "foo", "bar", "bar", "foo", "foo", "baz")))
 #  bar baz foo
 #    2   1   3
