c38e26771aba6cd815ba3aad1d3e8ea7b25b6c05
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 14:01:10 +0200

    summary

--- dataTypes/dataFrames.R
+++ dataTypes/dataFrames.R
@@ -26,3 +26,12 @@ df[-c(2,3)] # note the -, exclude columns 2 and 3
 # 1   1
 # 2   2
 # 3   3
+
+summary(df)
+#       foo           bar            baz
+#  Min.   :1.0   Min.   :11.0   Min.   :111.0
+#  1st Qu.:1.5   1st Qu.:16.5   1st Qu.:166.5
+#  Median :2.0   Median :22.0   Median :222.0
+#  Mean   :2.0   Mean   :22.0   Mean   :222.0
+#  3rd Qu.:2.5   3rd Qu.:27.5   3rd Qu.:277.5
+#  Max.   :3.0   Max.   :33.0   Max.   :333.0
