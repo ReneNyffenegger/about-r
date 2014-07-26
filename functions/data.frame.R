4e1c2ac036c835560e30d5bbd43182a3f71f46db
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 14:53:23 +0200

    2nd example

--- functions/data.frame.R
+++ functions/data.frame.R
@@ -10,5 +10,13 @@ show (dataFrame)
 # 2      Beta   2.020    Bar
 # 3     Gamma   3.003    Baz
 
+dataFrame_2 <- data.frame (
+                 col_1 = c("Foo", "Bar", "Baz"),
+                 col_2 = c(  11 ,   22 ,   33 ),
+                 col_3 = c("abc", "def", "ghi") )
+#   col_1 col_2 col_3
+# 1   Foo    11   abc
+# 2   Bar    22   def
+# 3   Baz    33   ghi
 
-
+show (dataFrame_2)
