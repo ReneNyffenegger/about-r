bff6c3316ffa93d0fa1da83415d48bd76bdc839d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 18:45:48 +0200

    + write.csv

---
+++ functions/write.csv.R
@@ -0,0 +1,14 @@
+col_1 <-  c(   1 ,    2 ,    3 )
+col_2 <-  c("foo", "bar", "baz")
+
+c <- cbind(col_1, col_2)
+
+colnames(c) <- c("Num", "Ident")
+
+write.csv(c, file="written.csv", row.names=FALSE)
+
+file.show("written.csv")
+# "Num","Ident"
+# "1","foo"
+# "2","bar"
+# "3","baz"
