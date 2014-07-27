1dee1b38c7f51df80ea0cf2a55b7cbfeaf955109
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-27 23:07:12 +0200

    names(v) <- ...

--- functions/names.R
+++ functions/names.R
@@ -3,3 +3,11 @@ l <- list(foo = 42, bar = 1:10, baz = 1:3 )
 n <- names(l)
 n
 # [1] "foo" "bar" "baz"
+
+# ----------------------------
+
+v <- 1:4
+names(v) <- c("one", "two", "three", "four")
+v
+#   one   two three  four
+#     1     2     3     4
