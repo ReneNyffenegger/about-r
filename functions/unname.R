99a4c7dcca0100cf04dc2c3c0fc9120bbd25beb7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-27 23:06:42 +0200

    + unname

---
+++ functions/unname.R
@@ -0,0 +1,12 @@
+v <- 1:4
+names(v) <- c("one", "two", "three", "four")
+v
+#   one   two three  four
+#     1     2     3     4
+
+unname(v)
+# [1] 1 2 3 4
+
+v
+#   one   two three  four
+#     1     2     3     4
