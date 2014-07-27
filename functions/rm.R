b611d33dc607966ead7e55b8338b5f2a7dfc47ae
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-27 22:44:23 +0200

    + rm

---
+++ functions/rm.R
@@ -0,0 +1,17 @@
+var_1 <- 42
+var_2 <- sqrt(var_1)
+var_3 <- var_2 * var_2
+var_4 <- var_3 * var_2
+
+ls()
+# [1] "var_1" "var_2" "var_3" "var_4"
+
+rm ( list = c("var_1", "var_3") )
+
+ls()
+# [1] "var_2" "var_4"
+
+rm ( list = ls() )
+
+ls()
+# character(0)
