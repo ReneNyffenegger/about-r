a69ac63e211cb3593f614d2eff5b87eb03084292
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-30 13:29:22 +0200

    + with

---
+++ functions/with.R
@@ -0,0 +1,12 @@
+df <- data.frame(foo=c(  1,  2,  3),
+                 bar=c( 11, 22, 33),
+                 baz=c(111,222,333))
+
+names(df)
+# [1] "foo" "bar" "baz"
+
+sum(df$bar)
+# [1] 66
+
+with(df, sum(bar))
+# [1] 66
