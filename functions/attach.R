cd3ff6a971deffd9e3e13862a2adde1179446743
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-30 13:30:12 +0200

    Interlink between attach and with

--- functions/attach.R
+++ functions/attach.R
@@ -1,3 +1,6 @@
+#
+#    Compare with -> with
+#
 df <- data.frame(foo=c(  1,  2,  3),
                  bar=c( 11, 22, 33),
                  baz=c(111,222,333))
--- functions/with.R
+++ functions/with.R
@@ -1,3 +1,6 @@
+#
+#    Compare with -> attach
+#
 df <- data.frame(foo=c(  1,  2,  3),
                  bar=c( 11, 22, 33),
                  baz=c(111,222,333))
@@ -10,3 +13,4 @@ sum(df$bar)
 
 with(df, sum(bar))
 # [1] 66
+
