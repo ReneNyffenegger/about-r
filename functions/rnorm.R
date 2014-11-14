fbe0b36aee1a7aa3624fec60eb8512e119723131
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-11-14 16:22:25 +0100

    use 51 breaks instead of 50

--- functions/rnorm.R
+++ functions/rnorm.R
@@ -3,7 +3,6 @@
 n <- rnorm(1000000, mean = 100, sd = 36)
 
 x11();
-hist(n, breaks = 50, col = "orange", main = "rnorm")
-
+hist(n, breaks = 51, col = "orange", main = "rnorm")
 
 z <- locator(1) # wait for mouse click or enter pressed
