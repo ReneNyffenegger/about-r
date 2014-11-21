3f6efab2563390bbc4648be4e7b7ebad8fd5cca1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-11-21 17:40:44 +0100

    pdf() and message()

--- functions/pdf.R
+++ functions/pdf.R
@@ -3,6 +3,10 @@ setwd("C:/temp")
 pdf ("test.pdf")
 
 hist(rnorm(10000, 0, 1), 20)
+
+# It seems that messages don't go into the pdf:
+message("Do messages work, too?")
+
 hist(rnorm(10000, 0, 4), 40)
 
 dev.off()
