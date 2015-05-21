ae6729458c75e2b55f25465ecd54181bba7da549
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-05-21 10:54:56 +0200

    use cat()

--- functions/read.csv.R
+++ functions/read.csv.R
@@ -2,19 +2,25 @@
 #    Compare with -> read.fwf (for reading «fixed width field» files).
 #
 
+
 # Assume data.csv with a header and values
 # to be seperated by commas (","):
 data <- read.csv("data.csv")
+
+cat("\n\n")
 
 show(data)
 #   col_1 col_2 col_3
 # 1   foo   bar   baz
 # 2   one   two three
 
+cat("\n\n\n")
+
 data$col_1
 # [1] foo one
 # Levels: foo one
 
+cat("\n\n\n")
 
 # Read a semicolon seperated file:
 data_ssv <- read.csv("data-no-header.ssv", head=FALSE, sep=";")
