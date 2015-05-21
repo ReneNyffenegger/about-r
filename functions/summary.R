86f65fc776076335d9155e4492aa6f879189ae8d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-05-21 09:18:32 +0200

    Use cat between calls to summary

--- functions/summary.R
+++ functions/summary.R
@@ -1,10 +1,16 @@
+cat ("\n\n")
+
 summary(c(  4, 9, 5, 1000, 11, 5, 8) )
 #  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
 #   4.0     5.0     8.0   148.9    10.0  1000.0
 
+cat ("\n\n\n")
+
 summary("Foo")
 #  Length     Class      Mode
 #       1 character character
+
+cat ("\n\n\n")
 
 # Summary of a Factor:
 #
