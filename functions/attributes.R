aa9efcabcb4f0ff905764c75033f1cd3ad6330d1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-05-21 11:04:33 +0200

    attributes() on dataFrames

--- functions/attributes.R
+++ functions/attributes.R
@@ -15,4 +15,15 @@ attributes(factor(c(1:10)))
 #  [1] "1"  "2"  "3"  "4"  "5"  "6"  "7"  "8"  "9"  "10"
 # 
 # $class
-[1] "factor"
+# [1] "factor"
+
+csv_data <- read.csv("data.csv")
+attributes(csv_data)
+# $names
+# [1] "col_1" "col_2" "col_3"
+# 
+# $class
+# [1] "data.frame"
+# 
+# $row.names
+# [1] 1 2
