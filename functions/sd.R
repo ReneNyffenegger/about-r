9a40c660208631c86ee8a6784830778f2464aa96
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-05-26 16:41:21 +0200

    comment

--- functions/sd.R
+++ functions/sd.R
@@ -1,8 +1,17 @@
 #
-#   Standard distribution
-#
-sd(rnorm(10000, 10, 4))
+#  Standard distribution (or deviation?)
+
+d <- rnorm(10000, 10, 4)
 #
 #  We expect a result of ca 4 because the 3rd 
 #  argument to rnorm is the standard deviation
 #
+sd(d)
+
+#
+#  The standard deviation can also be more verbosely
+#  be expressed like so:
+#
+m <- mean(d)
+sqrt(sum((d - m)^2) / (length(d)-1))
+
