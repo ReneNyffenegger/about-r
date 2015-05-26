9bdae272155bd69a298543a74f6a4ae6a46fb8df
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-05-26 17:13:45 +0200

    + rownames.R

---
+++ functions/rownames.R
@@ -0,0 +1,19 @@
+
+
+m <- matrix(1:30, nrow=10)
+
+rownames(m) <- LETTERS[1:10]
+colnames(m) <- c('foo', 'bar', 'baz')
+
+m
+#   foo bar baz
+# A   1  11  21
+# B   2  12  22
+# C   3  13  23
+# D   4  14  24
+# E   5  15  25
+# F   6  16  26
+# G   7  17  27
+# H   8  18  28
+# I   9  19  29
+# J  10  20  30
