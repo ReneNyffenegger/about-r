5f9e130bb57f3e04a0d1b0b11466acf2e52b3aee
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-02 13:46:13 +0200

    using attributes()

--- functions/attr.R
+++ functions/attr.R
@@ -11,3 +11,11 @@ str(obj)
 # atomic [1:3] Foo Bar Baz
 # - attr(*, "First attribute")= chr "Value for 1st attribute"
 # - attr(*, "Second attribute")= int [1:5] 1 2 3 4 5
+
+cat("\n\n")
+attributes(obj)
+# $`First attribute`
+# [1] "Value for 1st attribute"
+# 
+# $`Second attribute`
+# [1] 1 2 3 4 5
