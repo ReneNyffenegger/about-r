8da6f490eed6717503aafbdd001aa6c55bd2c020
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-19 17:54:32 +0200

    parameters xlim and ylim

--- functions/plot.R
+++ functions/plot.R
@@ -8,6 +8,8 @@ plot(x, y,
     sub ="plot.R"      , # Sub title 
     xlab="x"           , # x label
     ylab="f(x)"        , # y label,
+    xlim=c(-3,13)      , # range for x
+    ylim=c(-40,15)     , # range for y
     col ="blue"        , # color
     pch = 19           , # 19=solid
     type="p"             # type (p = points, also possible: 
@@ -24,6 +26,9 @@ plot(x, y,
 
 z <- locator(1) # wait for mouse click or enter pressed
 
+#
+#     Plot a function
+#
 plot (function(x) { x*x - 7*x - 20 },
       -2, 12)
 
