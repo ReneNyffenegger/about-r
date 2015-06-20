33f78f1312f1eb2ba3a47abaf8e54e87adf6a0de
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-20 13:28:31 +0200

    parameters 'by' and 'length'

--- functions/seq.R
+++ functions/seq.R
@@ -1,6 +1,15 @@
 #
 #  Compare with -> colon operator
 #
+
+
+seq(20, 41, by=4)
+# [1] 20 24 28 32 36 40
+
+seq(10, 26, length=4)
+# [1] 10.00000 15.33333 20.66667 26.00000
+
+
 x11(width=20,height=10)
 
 x <- seq(-5, 5, by=.1)
