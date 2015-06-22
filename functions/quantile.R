3bcc5047b8217691a9acda40cad76d4fa0ea7905
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-22 19:30:12 +0200

    15 percentile

--- functions/quantile.R
+++ functions/quantile.R
@@ -1,7 +1,12 @@
+#   See also
+#     -> summary
+#
+
 quantile(c(1, 10, 100, 1000, 2, 20, 200, 2000))
 #  0%  25%  50%  75% 100%
 #   1    8   60  400 2000
 
-
-#   See also
-#     -> summary
+cat("\n\n")
+quantile(100:200, .15) # the 15 percentile
+# 15%
+# 115
