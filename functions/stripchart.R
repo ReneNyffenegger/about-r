ba763a302ed3f656491f53f7313e77c8f7c4b07d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-22 18:47:40 +0200

    method='stack'

--- functions/stripchart.R
+++ functions/stripchart.R
@@ -11,3 +11,7 @@ foo <- c(10, 12, 17, 21)
 stripchart(foo, vertical=TRUE, method="jitter")
 
 z <- locator(1)
+
+bar <- c(17, 17, 17, 19, 22, 22, 22, 22, 22, 23, 23, 27, 27, 31, 31, 31)
+stripchart(bar, method='stack', xlab='Bar', pch=1, offset=1, cex=2)
+z <- locator(1)
