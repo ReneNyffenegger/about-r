55d00738fe757478953c665659c7208dcbe354ca
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-22 17:35:17 +0200

    More examples for barplot

--- functions/barplot.R
+++ functions/barplot.R
@@ -7,6 +7,7 @@ z <- locator(1) # wait for mouse click or enter pressed
 barplot(table(foo))
 z <- locator(1)
 
+# ---- Prices
 
 prices <- c(81, 76, 92)
 names(prices) <- c('Foo', 'Bar', 'Baz')
@@ -15,4 +16,12 @@ barplot(prices, main='Prices', ylab='Euros')
 z <- locator(1)
 
 barplot(prices, main='Prices', ylab='Euros', ylim=c(70, 95), xpd=FALSE) # xpd: Should bars be allowed to go outside region?
+z <- locator(1)
+
+
+# ---- Temperatures
+
+temperatures_august = rnorm(31, 18, 3.5)
+
+barplot(temperatures_august, names.arg=1:31, xlab="Day", ylab="Max Temp."
 z <- locator(1)
