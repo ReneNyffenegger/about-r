47781ebeb66e0071eee22e186ddc6a350b848654
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-22 18:08:21 +0200

    Temperatures

--- functions/barplot.R
+++ functions/barplot.R
@@ -1,4 +1,4 @@
-x11()
+x11(width="28")
 foo <- c(2, 1, 2, 4, 4, 1, 2, 2, 1, 3, 4, 2)
 
 barplot(foo)
@@ -21,7 +21,12 @@ z <- locator(1)
 
 # ---- Temperatures
 
-temperatures_august = rnorm(31, 18, 3.5)
+temperatures_august = round(rnorm(31, 18, 3.5), 1)
 
-barplot(temperatures_august, names.arg=1:31, xlab="Day", ylab="Max Temp."
+barplot(temperatures_august, names.arg=1:31, xlab="Day", ylab="Max Temp.")
+z <- locator(1)
+
+names(temperatures_august) <- 1:31
+y_coords = barplot(temperatures_august)
+text(y_coords, temperatures_august, labels=temperatures_august, pos=1)
 z <- locator(1)
