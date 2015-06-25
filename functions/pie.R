aa1178e1f1207914d83d7f539acc5038f822a455
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-25 17:59:29 +0200

    + pie

---
+++ functions/pie.R
@@ -0,0 +1,17 @@
+#
+#    Note: ?pie gives a clue as to why pie charts are
+#          a poor choice to convey information:
+#
+#         «Pie charts are a very bad way of displaying information.
+#          The eye is good at judging linear measures and bad at
+#          judging relative areas. A bar chart or dot chart is a
+#          preferable way of displaying this type of data.»
+#
+
+x11()
+
+sales <- c(51, 76, 92)
+names(sales) <- c('Foo', 'Bar', 'Baz')
+
+pie(sales, main="Sales") 
+z <- locator(1)
