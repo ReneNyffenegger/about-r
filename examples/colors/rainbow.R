9b174992a46f41f3a027ea147fabef6159c93a3b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-30 08:09:19 +0200

    + colors/rainbow.R

---
+++ examples/colors/rainbow.R
@@ -0,0 +1,16 @@
+X11()
+
+drawRainbow <- function(n) {
+
+  barplot(
+    rep(1,n),
+    col=rainbow(n),
+    yaxt='n')
+  
+  z <- locator(1)
+}
+
+drawRainbow (  4)
+drawRainbow ( 10)
+drawRainbow ( 20)
+drawRainbow ( 40)
