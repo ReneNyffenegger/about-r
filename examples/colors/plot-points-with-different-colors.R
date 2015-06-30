b5585be0e1b7fdcbe2f40f55c2be035c27f671e6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-30 08:28:31 +0200

    colors/plot-points-with-different-colors.R

---
+++ examples/colors/plot-points-with-different-colors.R
@@ -0,0 +1,11 @@
+X11()
+
+x <- -3:3
+y <- x^3 + 25*x^2-23*x
+
+plot(x,y,
+  pch=16,
+  col=c('palegreen', 'powderblue', 'seashell3', 'sandybrown', 'darkgrey', 'darkred', 'darkorange')
+)
+
+z <- locator(1)
