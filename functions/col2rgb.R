986003083d348ae96cfb596d21bcdbc02a011a35
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-01 09:12:58 +0200

    col2rgb called with a vector

--- functions/col2rgb.R
+++ functions/col2rgb.R
@@ -17,3 +17,11 @@ col2rgb("yellow")
 # green  255
 # blue     0
 
+
+cat ("\n\n")
+
+col2rgb(c('snow', 'orchid1', 'lightyellow'))
+#       [,1] [,2] [,3]
+# red    255  255  255
+# green  250  131  255
+# blue   250  250  224
