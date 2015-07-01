c7405c6e89678c85a510510b9448d3a75ff567e1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-01 11:38:16 +0200

    + margin.table

---
+++ functions/margin.table.R
@@ -0,0 +1,23 @@
+m <- matrix (
+       c(    4, 1, 3,
+             2, 1, 2,
+             1, 5, 2,
+             4, 3, 1),
+       ncol  = 3,
+       byrow = TRUE
+)
+
+margin.table(m)
+# [1] 29
+sum(m)
+# [1] 29
+
+margin.table(m, 1)
+# [1] 8 5 8 8
+apply(m, 1, sum)
+# [1] 8 5 8 8
+
+margin.table(m, 2)
+# [1] 11 10  8
+apply(m, 2, sum)
+# [1] 11 10  8
