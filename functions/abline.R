8c50113fc90128fd76fc6449a557c08c61ef0a1f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-01 22:24:34 +0200

    draw horizontal line

--- functions/abline.R
+++ functions/abline.R
@@ -1,8 +1,21 @@
+x11()
+
+x <- 1:10
+y <- x^2
+
+plot(x, y)
+
+
+abline(h = mean  (y), col='blue') # h stands for horizontal
+abline(h = median(y), col='red' )
+
+
+z <- locator(1) # wait for mouse click or enter pressed
+
 x <- seq(-5, 5, 0.5)
 
 y <- 0.4*x*x + 4*x + 3 + runif(length(x), -2, 2)
 
-x11()
 
 plot(x,y)
 
@@ -11,4 +24,3 @@ fit <- lm(y~x)
 abline(fit, col=2)
 
 z <- locator(1) # wait for mouse click or enter pressed
-
