9f5a6a0fa64b2df14dc81e32c3198191dab5d74e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-02 22:16:55 +0200

    + methods

---
+++ functions/methods.R
@@ -0,0 +1,9 @@
+methods("predict")
+#  [1] predict.ar*                predict.Arima*
+#  [3] predict.arima0*            predict.glm
+#  [5] predict.HoltWinters*       predict.lm
+#  [7] predict.loess*             predict.mlm*
+#  [9] predict.nls*               predict.poly*
+# [11] predict.ppr*               predict.prcomp*
+# [13] predict.princomp*          predict.smooth.spline*
+# [15] predict.smooth.spline.fit* predict.StructTS*
