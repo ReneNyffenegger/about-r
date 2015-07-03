7a8eea30aeb71bee21cec428fe01c3a816c522ce
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-03 04:41:39 +0200

    + fitted, resid

---
+++ functions/resid.R
@@ -0,0 +1,22 @@
+#
+#    See also -> fitted
+#
+x <- c(25, 23, 22, 18, 29, 19, 24, 28, 21, 26, 23, 31, 26, 27, 30, 24, 22)
+
+A =   3
+B =  -4
+E = rnorm(length(x), mean = 0, sd = 3)
+
+y <- A + B*x + E
+
+X11()
+plot (x, y, col='red', pch=16)
+
+fit <- lm (y~x)
+
+r <- resid(fit)
+
+points(x, y-r, col='blue', pch=16)
+abline(coef(fit)[1], coef(fit)[2], col='blue')
+
+z <- locator(1)
