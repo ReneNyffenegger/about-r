0d5542f32ca835d5e2abb065d7abdadf317b088e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-08 16:38:27 +0200

    using which to find the 'longest string'

--- functions/which.R
+++ functions/which.R
@@ -1,2 +1,10 @@
 which(c( F, T, T, F, F, F, T, F, T))
 #[1] 2 3 7 9
+
+# find longest name:
+names = c('foo', 'bar baz', 'a longer foo than all other bazes', 'bar bazed on his own foo')
+
+names_len = nchar(names)
+
+names[which(names_len == max(names_len))]
+# [1] "a longer foo than all other bazes"
