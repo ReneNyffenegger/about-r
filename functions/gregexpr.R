af8b9232e957519e6777ad6b0a25bad3f509c89d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-08 16:51:20 +0200

    + gregexpr

---
+++ functions/gregexpr.R
@@ -0,0 +1,19 @@
+str <- c(
+  'Foo Bar Baz',
+  'one two three four five'
+)
+
+gregexpr("o", str)
+# [[1]]
+# [1] 2 3
+# attr(,"match.length")
+# [1] 1 1
+# attr(,"useBytes")
+# [1] TRUE
+# 
+# [[2]]
+# [1]  1  7 16
+# attr(,"match.length")
+# [1] 1 1 1
+# attr(,"useBytes")
+# [1] TRUE
