164e59192728357f8a4166433b9986f8ca6d056d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-08 11:09:08 +0200

    Using different pch and col

--- functions/plot.R
+++ functions/plot.R
@@ -3,6 +3,8 @@ y <- x*x - 7*x - 20
 
 x11()
 
+# { first example
+
 plot(x, y, 
     main="Plot Example", # Title
     sub ="plot.R"      , # Sub title 
@@ -26,10 +28,29 @@ plot(x, y,
 
 z <- locator(1) # wait for mouse click or enter pressed
 
-#
-#     Plot a function
-#
+# }
+
+# {   Plot a function
+
 plot (function(x) { x*x - 7*x - 20 },
       -2, 12)
 
 z <- locator(1) # wait for mouse click or enter pressed
+
+# }
+
+# { Using different «pch» and «col» for the «dots»
+
+
+d <- data.frame(
+  
+      foo = c(1.1, 2.0, 2.7, 2.4, 1.4, 3.3, 2.5, 1.7, 3.6, 3.3, 2.3, 3.6, 1.9, 3.8, 2.5),
+      bar = c(2.9, 1.8, 2.6, 1.5, 3.3, 3.2, 1.1, 2.0, 2.9, 1.2, 2.5, 1.4, 3.1, 2.0, 1.2),
+      baz = c(1  , 1  , 3  , 2  , 1  , 2  , 3  , 2  , 1  , 2  , 1  , 2  , 3  , 2  , 2  )
+)
+
+plot(d$foo, d$bar, pch=d$baz, col=c('red', 'blue', 'darkgreen')[d$baz])
+
+z <- locator(1)
+
+# }
