ddf098ac67f6c1bdb4656e58cd0e9de712349003
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-14 11:49:01 +0200

    + text.R

--- functions/text.R
+++ functions/text.R
@@ -5,8 +5,24 @@ y = x^2
 
 t = paste0(x, "^2 = ", y)
 
-plot(x, y, pch=19, col="red" )
-text(x, y, t     , col="blue", adj=c(-0.2, -0.2), cex=0.8)
+par(xpd=TRUE) # Enable text to be drawn outside the plot region.
 
+margins_orig = as.numeric(unlist(par()['mar']))
+margins_     = margins_orig+c(0, 0, 0.1, 2) # extend top by 0.1, right by 2 
+par (mar = margins_)  
+# par  (usr = c(min(x)-100, max(x)+10, min(y), max(y)+ 0.1))
+
+plot(x, y, pch=19, col='red',
+     bty='n'  # Suppres box around plot
+#    xlim=c(min(x), max(x)),
+#    ylim=c(min(y), max(y))
+#    yaxp=c(
+#      floor  (min(y)),
+#      ceiling(max(y)),
+#      5
+#   )
+)
+
+text(x, y, t, col='blue', adj=c(-0.2, -0.2), cex=0.8, bg='green')
 
 z <- locator(1)
