c3c7298656e5dda8e354ce6cceab64e24876d631
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-14 09:12:36 +0200

    + *.time for dist

--- functions/system.time.R
+++ functions/system.time.R
@@ -6,3 +6,14 @@ system.time(cat("\nHello World\n"))
 # Hello World
 #    user  system elapsed
 #       0       0       0
+
+cat ("\n\n")
+
+set.seed(17)
+nof_columns =  120
+nof_rows    = 4500
+m <- matrix( rnorm (nof_columns * nof_rows), nrow = nof_rows)
+
+system.time( dist(m) )
+#    user  system elapsed
+#    7.55    0.02    7.61
--- functions/unix.time.R
+++ functions/unix.time.R
@@ -6,3 +6,14 @@ unix.time(cat("\nHello World\n"))
 # Hello World
 #    user  system elapsed
 #       0       0       0
+
+cat ("\n\n")
+
+set.seed(17)
+nof_columns =  120
+nof_rows    = 4500
+m <- matrix( rnorm (nof_columns * nof_rows), nrow = nof_rows)
+
+unix.time( dist(m) )
+#    user  system elapsed
+#    7.65    0.03    7.69
