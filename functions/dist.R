b114c22cc0edea8ef58173ecca1002a6d93557fe
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-14 08:58:46 +0200

    some simpler examples, imho

--- functions/dist.R
+++ functions/dist.R
@@ -1,3 +1,31 @@
+a = c(0, 0, 0)
+b = c(0, 0, 3)
+c = c(0, 4, 3)
+
+dist(rbind(a, b))
+#   a
+# b 3
+
+cat("\n\n")
+
+abc <- rbind (a, b, c)
+dist(abc)
+#   a b
+# b 3
+# c 5 4
+
+# Interpretation:
+# A's (euclidian) distance from b is 3, from c is 5.
+# B's (euclidian) distance from c is 4
+
+cat("\n\n")
+dist(abc, 'manhattan')
+#   a b
+# b 3
+# c 7 4
+
+# -----------------------------------------
+
 m <- matrix ( 
       c( 2, 1, 8, 5,
          7, 6, 3, 4,
@@ -9,6 +37,7 @@ m <- matrix (
 
 rownames(m) <- c('A', 'B', 'C', 'D')
 
+cat("\n\n")
 dist(m)
 #           A         B         C
 # B  8.717798
