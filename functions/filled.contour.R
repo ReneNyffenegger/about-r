7a54f12eae0affcdf151d67000fdd7a69dbb5827
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-23 17:00:11 +0200

    Adding another example

--- functions/filled.contour.R
+++ functions/filled.contour.R
@@ -13,3 +13,26 @@ filled.contour(
 )
 
 z <- locator(1)
+
+
+x <- seq(-10, 10, length=200)
+y <- seq(-10, 10, length=200)
+
+z <- matrix (
+  outer(x, y,
+        function(x, y) {
+          14 - 
+          sqrt( (x+cos(y)/10) ^2 + (y - sin(x)/5) ^2) +
+          cos((x-2)/3)*sin(x) + 
+          cos((y-2)/4)*cos(y) +
+        # 
+          sqrt( (x+3) ^ 2 + (y/2-2) ^2)
+        }
+  ),
+  nrow=length(x)
+)
+
+# filled.contour(z)
+filled.contour(x,y,z,col=rainbow(200),nlevels=200)
+
+locator(1)
