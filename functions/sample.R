de6538e8fd410bbb5ecab37c4877b06fcec9ca0c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-27 15:38:22 +0200

    + prob parameter

--- functions/sample.R
+++ functions/sample.R
@@ -4,3 +4,8 @@ sample(1:10)
 
 # Generate 100 zeroes or ones:
 sample(c(0,1), 100, replace = TRUE)
+
+cat ("\n\n")
+
+# Make zeroes much more probable:
+sample(c(0,1), 100, replace = TRUE, prob=c(0.9, 0.1))
