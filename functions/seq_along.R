30dd136cb5f3800f7f6d8202ea3b178538cf2ad0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-09-07 13:21:19 +0200

    Link to http://stackoverflow.com/questions/13732062/seq-vs-seq-along-when-will-using-seq-cause-unintended-results

--- functions/seq_along.R
+++ functions/seq_along.R
@@ -4,7 +4,7 @@ seq_along(4:10)
 seq_along(4)
 # [1] 1
 
-# Compare to seq()
+# Compare to seq()  -- s.a. http://stackoverflow.com/questions/13732062/seq-vs-seq-along-when-will-using-seq-cause-unintended-results
 seq(4:10)
 # [1] 1 2 3 4 5 6 7
 
