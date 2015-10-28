30b5a55113dda15814fdb4fc11b856a5120653b3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-28 18:00:47 +0100

    dput / dget

---
+++ functions/dget.R
@@ -0,0 +1,6 @@
+#
+#   First run -> dput()
+#
+d_ <- dget('dput.out')
+
+print (d_)
---
+++ functions/dput.R
@@ -0,0 +1,8 @@
+d = data.frame (
+      r1 = rnorm(100),
+      r2 = rnorm(100)
+)
+
+# print(d)
+
+dput(d, 'dput.out')
