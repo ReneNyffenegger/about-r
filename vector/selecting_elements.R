d73c129d92e03f02b57ab828f529dad37ebb5140
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-17 23:17:59 +0200

    Select elements with TRUE and FALSE

--- vector/selecting_elements.R
+++ vector/selecting_elements.R
@@ -11,3 +11,11 @@ vec[8:10]
 # Exclude specific elements (2, 4 and 6)
 vec[c(-2,-4,-6)]
 # [1]  1  3  5  7  8  9 10 11 12 13 14 15 16 17 18 19 20
+
+# Select elements with TRUE (and excluding others with FALSE):
+vec[c(FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, FALSE)]
+# [1]  2  3  5  6  7  8  9 10 11 12 13 14 15 17 18 19
+
+# Same idea, but generating TRUE/FALSE
+vec[vec <= 10]
+#  [1]  1  2  3  4  5  6  7  8  9 10
