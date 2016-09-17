2a65fe53c09a93033676834c6f850a68e5be8954
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-17 22:08:19 +0200

    print a matrix

--- functions/print.R
+++ functions/print.R
@@ -14,4 +14,9 @@ print("foo\"bar")
 print("foo\"bar", quote=FALSE)
 # [1] foo"bar
 
+print(matrix(c('foo', 1, 2, 'bar', 3, 4), 2, 3))
+#      [,1]  [,2]  [,3]
+# [1,] "foo" "2"   "3"
+# [2,] "1"   "bar" "4"
+
 # See also cat
