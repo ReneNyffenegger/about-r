630ee291bd0a26647a9bd40de3fa67ed3dc2a5cb
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-17 23:26:33 +0200

    Selecting elements by name

--- vector/selecting_elements.R
+++ vector/selecting_elements.R
@@ -1,21 +1,31 @@
-vec <- 1:20
+vec <- 1:10
 
 # Note: index is 1 based, not 0 based (as for example in
 # Perl or C)
 vec[4]
 # [1] 4
 
-vec[8:10]
-# [1]  8  9 10
+vec[7:9]
+# [1] 7 8 9
 
 # Exclude specific elements (2, 4 and 6)
 vec[c(-2,-4,-6)]
-# [1]  1  3  5  7  8  9 10 11 12 13 14 15 16 17 18 19 20
+# [1]  1  3  5  7  8  9 10
 
 # Select elements with TRUE (and excluding others with FALSE):
-vec[c(FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, FALSE)]
-# [1]  2  3  5  6  7  8  9 10 11 12 13 14 15 17 18 19
+vec[c(FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE)]
+# [1]  2  5  6  7  8 10
 
 # Same idea, but generating TRUE/FALSE
-vec[vec <= 10]
-#  [1]  1  2  3  4  5  6  7  8  9 10
+vec[vec <= 5]
+# [1] 1 2 3 4 5
+
+# Select all elements that are greater than the mean of the vector:
+vec[vec > mean(vec)]
+# [1]  6  7  8  9 10
+
+# selecting elements by name
+names(vec) <- c('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten')
+vec[c('two', 'seven', 'three')]
+#   two seven three
+#     2     7     3
