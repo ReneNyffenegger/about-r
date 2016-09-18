e49dc156fa7f314de90babab4a358c039fd16108
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-18 21:04:50 +0200

    print values of df_1 / df_2

--- functions/rbind.R
+++ functions/rbind.R
@@ -22,6 +22,19 @@ df_2 <- data.frame(
   col_2 = c(    1,    2 ,     3  )
 )
 
+df_1
+#   col_1 col_2
+# 1   Foo    11
+# 2   Bar    22
+# 3   Baz    33
+
+df_2
+#   col_1 col_2
+# 1   one     1
+# 2   two     2
+# 3 three     3
+
+
 #
 #  Note, column names bust be the same for rbind to work,
 #  otherwise «names do not match previous names» error is
