7a498fd2af9714bb8f18cb7143c9f7d0bc45d125
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-18 18:46:05 +0200

    + names_property

---
+++ vector/names_property.R
@@ -0,0 +1,9 @@
+vec <- 1:5
+names(vec) <- c('one', 'two', 'three', 'four', 'five')
+vec
+#   one   two three  four  five
+#     1     2     3     4     5
+
+vec[c('two', 'five')]
+#  two five
+#    2    5
