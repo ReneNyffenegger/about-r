8fd2eea9398b07e058e2fbc77afdadc209aaf6fd
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-18 13:54:52 +0200

    + tempfile

---
+++ functions/tempfile.R
@@ -0,0 +1,11 @@
+# tempfile() does not generate a file, it returns
+# the name for a usable file:
+temp_file_name <- tempfile()
+
+file_handle <- file(temp_file_name)
+
+cat("\nWriting to ", temp_file_name, '\n')
+writeLines(c('first line', "second line"), file_handle)
+
+cat("\nshowing content of file:\n")
+file.show(temp_file_name)
