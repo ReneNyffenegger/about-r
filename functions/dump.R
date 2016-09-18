53b93b0d25008674e74ebb5544c60a05b16480cc
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-18 18:35:57 +0200

    Show content of dumped file

--- functions/dump.R
+++ functions/dump.R
@@ -1,12 +1,18 @@
 v <- c(5, 2, 99, 42)
 w <- matrix( c(1, 2, 3, 4, 5, 6), nrow=2, ncol=3)
 
-dump(c("v", "w"), "c:\\temp\\dumped")
+dump(c("v", "w"), "dumped")
 
 v <- NA
 w <- NA
 
-source("C:\\temp\\dumped")
+file.show("dumped")
+# v <-
+# c(5, 2, 99, 42)
+# w <-
+# structure(c(1, 2, 3, 4, 5, 6), .Dim = 2:3)
+
+source("dumped")
 
 v
 # [1]  5  2 99 42
