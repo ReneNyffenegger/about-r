262bfb747a0f24568065d315ec7d8f7081b6d3da
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-18 14:06:24 +0200

    sleeping a few seconds

--- functions/tempfile.R
+++ functions/tempfile.R
@@ -9,3 +9,10 @@ writeLines(c('first line', "second line"), file_handle)
 
 cat("\nshowing content of file:\n")
 file.show(temp_file_name)
+
+cat("\n\nGoing to sleep for a few seconds so that")
+cat("\nthe content of the file can be checked.")
+cat("\nThe file will be destroyed after the script finishes")
+Sys.sleep(20)
+
+close(file_handle)
