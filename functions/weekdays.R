56df76a767b4026b0878372e9ef19335b4dd0675
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-09-23 20:10:10 +0200

    weekdays for a range

--- functions/weekdays.R
+++ functions/weekdays.R
@@ -2,4 +2,7 @@ dev_null <- Sys.setlocale("LC_TIME", "english")
 
 weekdays( c(ISOdate(1970, 8, 28),
             ISOdate(2016, 9, 21)))
-[1] "Friday"    "Wednesday"
+# [1] "Friday"    "Wednesday"
+
+weekdays( ISOdate(2000, 1, 1) + 0:6)
+# [1] "Saturday" "Saturday" "Saturday" "Saturday" "Saturday" "Saturday" "Saturday"
