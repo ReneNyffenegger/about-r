3d4c87f071c0060ef1e5930bfb218b6b07b44706
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-12 07:42:34 +0200

    alternative calculation

--- functions/choose.R
+++ functions/choose.R
@@ -1,7 +1,22 @@
 #
-#     How many sets with 2 elements can I choose from a set
-#     with 4 elements:
+#     How many sets with n elements can I choose from a set
+#     with k elements:
 #
 
-choose(4,2)
-# [1] 6
+n <- 3
+k <- 7
+
+choose(k, n)
+# [1] 35
+
+# Alternative calculation:
+a <- 1
+b <- 1
+
+for (i in 1:n) {
+  a <- a *  i
+  b <- b * (k-i+1)
+}
+
+paste("alternatively calculated:",b/a)
+# [1] "alternatively calculated: 35"
