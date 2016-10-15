88224df5dafe2ee699c51e9a8df3bd23cafbd18e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-15 19:28:20 +0200

    + dbinom

---
+++ functions/dbinom.R
@@ -0,0 +1,14 @@
+dbinom(1, size=1, prob=0.85)
+# [1] 0.85
+
+dbinom(0, size=1, prob=0.85)
+# [1] 0.15
+
+dbinom (0, size=2, prob=0.5)
+# [1] 0.25
+
+dbinom (1, size=2, prob=0.5)
+# [1] 0.5
+
+dbinom (2, size=2, prob=0.5)
+# [1] 0.25
