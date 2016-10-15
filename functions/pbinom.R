18a449ea743ba176ed7a25ad621b7c697a7995cf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-15 19:31:41 +0200

    + pbinom

---
+++ functions/pbinom.R
@@ -0,0 +1,12 @@
+dbinom(0, size=10, prob=0.744) +
+dbinom(1, size=10, prob=0.744) +
+dbinom(2, size=10, prob=0.744) +
+dbinom(3, size=10, prob=0.744) +
+dbinom(4, size=10, prob=0.744) +
+dbinom(5, size=10, prob=0.744) +
+dbinom(6, size=10, prob=0.744) +
+dbinom(7, size=10, prob=0.744)
+# [1] 0.4923509
+
+pbinom(7, size=10, prob=0.744)
+# [1] 0.4923509
