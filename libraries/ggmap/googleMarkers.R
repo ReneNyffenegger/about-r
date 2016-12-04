f95f8157a95066118e6df8a7b939bfa4524d576a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-05 00:44:50 +0100

    + googleMarkers.R

---
+++ libraries/ggmap/googleMarkers.R
@@ -0,0 +1,28 @@
+library(ggmap)
+
+x11()
+
+set.seed(28870)
+
+zuerich = geocode('Zürich')
+
+rx=0.03
+ry=0.02
+
+coords <- data.frame(
+  zuerich$lon + jitter(rx*sin(seq(0, 6, length=15)), 35),
+  zuerich$lat + jitter(ry*cos(seq(0, 6, length=15)), 35)
+)
+
+
+map <- get_googlemap(
+          center  = c(lon=zuerich$lon, lat=zuerich$lat),
+          markers = coords,
+          path    = coords,
+          scale   = 2,
+          zoom    =13)
+
+ggmap(map, extent = 'device')
+
+# TODO: http://stackoverflow.com/questions/40963761/how-do-i-make-an-r-script-wait-for-a-mouse-click
+Sys.sleep(10)
