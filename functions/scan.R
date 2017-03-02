c357721532572eae444b5e8904ac12d42a6268d2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-02 18:40:41 +0100

    scanning from a textConnection

--- functions/scan.R
+++ functions/scan.R
@@ -7,3 +7,24 @@ s <- scan(file="scan.input")
 
 s
 # [1] 10.0 42.4 19.0  5.0  3.0 11.0 12.2 -6.0  4.0
+
+mode(s)
+# [1] "numeric"
+
+#
+#   Scanning from a (text-)connection
+#   Note the second argument to scan that is
+#   the type of items being read (»character()«)
+#
+s <- scan(textConnection('  one  two
+  three    four  five
+six   seven      eight
+    nine     ten     '), character())
+# Read 10 items
+
+mode(s)
+# [1] "character"
+
+s
+#  [1] "one"   "two"   "three" "four"  "five"  "six"   "seven" "eight" "nine"
+# [10] "ten"
