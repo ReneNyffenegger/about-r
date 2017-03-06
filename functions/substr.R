207840805f53e9b61f92856b6d1fea24d32f7cf3
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-06 14:40:10 +0100

    better examples

--- functions/substr.R
+++ functions/substr.R
@@ -1,26 +1,36 @@
 #
-#   S.a. -> substring()
-#        ../examples/basic_string_manipulation.R
+#   S.a. 
+#     ../examples/basic_string_manipulation.R
 #
 #
 
-letters_ = paste0(letters, collapse="")
-substr(letters_, 10, 13)
-# [1] "jklm"
+text = '1234567890'
+substr(text, 0, 5)
+# [1] "12345"
 
-letters_
-# [1] "abcdefghijklmnopqrstuvwxyz"
+substr(text, 1, 5)
+# [1] "12345"
 
-substr(letters_, 10, 13) <- '#'
-letters_
-# [1] "abcdefghi#klmnopqrstuvwxyz"
+substr(text, 2, 5)
+# [1] "2345"
 
-substr(letters_,  4,  7) <- '____'
-letters_
-# [1] "abc____hi#klmnopqrstuvwxyz"
+substr(text, -4, 3)
+# [1] "123"
+
+substr(text, 3, 3)
+# [1] "3"
+
+substr(text, 3, 7) <- 'xy'
+text
+# [1] "12xy567890"
+
+substr(text, 6, 7) <- 'abcd'
+text
+# [1] "12xy5ab890"
+
 
 # With recycling
-
 a <- c('foo', 'bar', 'baz', 'one', 'two', 'three')
-substr( a , 2, 2) <- c(',', '_')
+substr(a , 2, 2) <- c(',', '_')
 a
+# [1] "f,o"   "b_r"   "b,z"   "o_e"   "t,o"   "t_ree"
