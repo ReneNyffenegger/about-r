8e83e19b10df24cc1f480eee4b84786b1bb5e836
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-08 10:40:29 +0100

    More comment

--- functions/png.R
+++ functions/png.R
@@ -1,12 +1,15 @@
 
-x <- rnorm(10000)
 
+#   Create first png file
+#
+x <- rnorm(10000)
 png(file='rnorm_300x200.png', width=300, height=200)
 hist(x)
 invisible(dev.off())
 
+#   Create second png file
+#
 x <- rnorm(50000)
-
 png(file='rnorm_600x400.png', width=600, height=400)
 hist(x)
 invisible(dev.off())
