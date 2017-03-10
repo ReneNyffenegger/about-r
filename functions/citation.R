6cee7801b56ce41a3ef284cea9b7f5eee6517b5d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-10 16:58:12 +0100

    + citation.R

---
+++ functions/citation.R
@@ -0,0 +1,17 @@
+citation('oce')
+# 
+# To cite package ‘oce’ in publications use:
+# 
+#   Dan Kelley and Clark Richards (2016). oce: Analysis of Oceanographic
+#   Data. R package version 0.9-20.
+#   https://CRAN.R-project.org/package=oce
+# 
+# A BibTeX entry for LaTeX users is
+# 
+#   @Manual{,
+#     title = {oce: Analysis of Oceanographic Data},
+#     author = {Dan Kelley and Clark Richards},
+#     year = {2016},
+#     note = {R package version 0.9-20},
+#     url = {https://CRAN.R-project.org/package=oce},
+#   }
