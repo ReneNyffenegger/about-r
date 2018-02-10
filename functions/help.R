135110979a3d2a4b955ae2e50e591ecf1165decd
René Nyffenegger <mail@renenyffenegger.ch>
2018-02-10 19:06:31 +0100

    reformat

--- functions/help.R
+++ functions/help.R
@@ -4,10 +4,11 @@ help(help)
 
 help(help, help_type="text")
 help(help, help_type="html")
-help(help, help_type="pdf")
+help(help, help_type="pdf" )
 
 # Get help for a package
 help(package="nnet")
 
-# Shortcut
+# Shortcut:
+#  help can be abbreviated with a question mark:
 ?ls
