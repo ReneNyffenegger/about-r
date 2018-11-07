a99262b8f1badbe37e88cdff16184efdedbbe96e
René Nyffenegger <mail@renenyffenegger.ch>
2018-11-07 20:09:44 +0100

    Add default parameter, remove min()

--- functions/max.R
+++ functions/max.R
@@ -1,17 +1,17 @@
-min(c(5, 10, 3, 9, 20))
-# [1] 3
-
-
 max( c( 4, 9, 7, 12, 3, 6) )
 # [1] 12
 
-max( c( NA, NA, 5, 6, 4, NA ), na.rm=TRUE  )
+vector_with_NA = c( NA, NA, 5, 6, 4, NA )
+
+
+max( vector_with_NA, na.rm=TRUE  )
 # [1] 6
 
-max( c( NA, NA, 5, 6, 4, NA ), na.rm=FALSE )
+max( vector_with_NA, na.rm=FALSE )
 # [1] NA
 
-
 #
-#   See also
-#     -> summary
+# If the parameter na.rm is not specified, it defaults to false:
+#
+max( vector_with_NA )
+# [1] NA
