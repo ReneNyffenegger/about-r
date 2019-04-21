5e581cc08fea51816407ecf0540254581ad2837b
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-21 06:59:24 +0200

    Align paranthesis

--- functions/read.table/text.R
+++ functions/read.table/text.R
@@ -16,14 +16,14 @@ df
 # 2  bar 123456789 -28.2
 # 3  baz         0  99.9
 
-class (df$name)   # factor
+class (df$name )  # factor
 class (df$val_1)  # integer
 class (df$val_2)  # numeric
 
-mode  (df$name)   # numeric
+mode  (df$name )  # numeric
 mode  (df$val_1)  # numeric
 mode  (df$val_2)  # numeric
 
-typeof(df$name)   # integer
+typeof(df$name )  # integer
 typeof(df$val_1)  # integer
 typeof(df$val_2)  # double
