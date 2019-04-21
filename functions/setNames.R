1d0e1b43d489bead3d9d1f7b60b919531c7a947d
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-21 21:24:58 +0200

    + setNames.R

---
+++ functions/setNames.R
@@ -0,0 +1,14 @@
+vec_a <- setNames(c( 42, 99, 3, 18, 5), c('val_1', 'val_2', 'val_3', 'val_4', 'val_5'))
+
+vec_a
+#
+# val_1 val_2 val_3 val_4 val_5 
+#    42    99     3    18     5 
+
+vec_b <- c( 5, 27, 13, 48, 19)
+names(vec_b) <- c('val_1', 'val_2', 'val_3', 'val_4', 'val_5')
+
+vec_b
+#
+# val_1 val_2 val_3 val_4 val_5 
+#     5    27    13    48    19
