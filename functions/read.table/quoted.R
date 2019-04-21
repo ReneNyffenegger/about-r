7b304abfe26c55631cbebe25f2b234f1060d8169
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-21 13:29:02 +0200

    + quoted.R

---
+++ functions/read.table/quoted.R
@@ -0,0 +1,13 @@
+df <- read.table(
+  text="name val_1
+'item one' 1.1
+'item two' 2.22
+'item three' 3.333",
+header = TRUE)
+
+df
+#
+#         name val_1
+# 1   item one 1.100
+# 2   item two 2.220
+# 3 item three 3.333
