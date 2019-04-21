7a41eecf4fb90c9c2b5e00abd4e0d53da12fab41
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-21 06:31:30 +0200

    Use map rather than n

--- packages/rworldmap/highlight-countries-in-europe.R
+++ packages/rworldmap/highlight-countries-in-europe.R
@@ -6,7 +6,7 @@ countries <- data.frame(
       foo     = c(   1 ,    3 ,    4 ,    1 ,    1 ,    2 ,    2 ,    3 ,    3,     2 )
 )
 
-n <- joinCountryData2Map(
+map <- joinCountryData2Map(
        countries,
        joinCode       = "ISO3", # ISO 3166.1 / alpha 3
        nameJoinColumn = "country"
@@ -14,7 +14,7 @@ n <- joinCountryData2Map(
 
 png('img/highlight-countries-in-europe.png', width=400, height=400)
 par(mar=c(0, 0, 1, 0))
-mapCountryData(n,
+mapCountryData(map,
    nameColumnToPlot  = 'foo'        ,
    catMethod         = 'categorical',
    mapRegion         = 'Europe'     ,
