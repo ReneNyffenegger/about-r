aeabae0bd7c088ecfd073bf3464a1fa6a0e8bad6
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-27 15:33:46 +0200

    + assign-id-with-rownumber.R

---
+++ packages/dplyr/assign-id-with-rownumber.R
@@ -0,0 +1,10 @@
+library(dplyr)
+
+df <- data.frame(
+  col_1 = c('foo', 'bar', 'foo', 'baz'),
+  col_2 = c( 7.2 ,  1.8 ,  2.0 ,  4.1 )
+)
+
+df                          %>%
+  mutate(id = row_number()) %>%  # Assign (unique) id with row_number
+  select(id, col_1, col_2 )      # Re order columns
