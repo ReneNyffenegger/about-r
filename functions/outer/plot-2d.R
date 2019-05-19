042e5f6711ff43f431500418efbe487b699bd632
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-05-19 08:02:04 +0200

    + plot-2d

---
+++ functions/outer/plot-2d.R
@@ -0,0 +1,11 @@
+x <- y <- seq(-1, 1, len=51);
+
+grid <- outer(
+        x,
+        y,
+        function(x, y) {
+           sin(x) * exp(y)
+        }
+      );
+
+filled.contour(grid, axes = FALSE, color.palette=rainbow, asp=1);
