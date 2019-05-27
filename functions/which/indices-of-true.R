c5a548c79fea5d2b07d201c637a8e846896fbb96
René Nyffenegger <mail@renenyffenegger.ch>
2019-05-27 12:05:00 +0200

    + indices-of-true.R

---
+++ functions/which/indices-of-true.R
@@ -0,0 +1,3 @@
+which(c( F, T, T, F, F, F, T, F, T))
+#
+#  2 3 7 9
--- functions/which/which.R
+++ functions/which/which.R
@@ -1,6 +1,3 @@
-which(c( F, T, T, F, F, F, T, F, T))
-#[1] 2 3 7 9
-
 # find longest name:
 names = c('foo', 'bar baz', 'a longer foo than all other bazes', 'bar bazed on his own foo')
 
