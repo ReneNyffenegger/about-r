38060b32f153f7098636a2ca5e2ccaa032ea1078
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-05-30 19:57:55 +0200

    geonamesHost

--- packages/geonames/search-Wagenburg-CH.R
+++ packages/geonames/search-Wagenburg-CH.R
@@ -1,4 +1,6 @@
-library(geonames)
 options(geonamesUsername=readline(prompt='Enter username for geonames.org: '));
+options(geonamesHost="api.geonames.org");
+
+library(geonames)
 
 GNsearch(name = 'Wagenburg', country = 'CH');
