dfb9863d31c0147279a428e51dc72020cf57ac9a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-06-16 17:53:54 +0200

    identical

--- functions/identical.R
+++ functions/identical.R
@@ -1,17 +1,13 @@
-#
-#   S.a. ../examples/set_operations.R
-#
-
-set_a <- c('Bar', 'Baz', 'Foo'  )
-set_b <- c('Baz', 'Foo', 'Bar'  )
-set_c <- c('Bar', 'Baz', 'Foo'  )
-set_d <- c('Bar', 'Baz', 'Foo', 'Foo', 'Foo')
+set_a <- c('Bar', 'Baz', 'Foo'              );
+set_b <- c('Baz', 'Foo', 'Bar'              );
+set_c <- c('Bar', 'Baz', 'Foo'              );
+set_d <- c('Bar', 'Baz', 'Foo', 'Foo', 'Foo');
 
 identical(set_a, set_b)
-# [1] FALSE
+# FALSE
 
 identical(set_a, set_c)
-# [1] TRUE
+# TRUE
 
 identical(set_a, set_d)
-# [1] FALSE
+# FALSE
