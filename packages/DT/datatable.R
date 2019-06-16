c1264ba1fbdd9f65cd86214e9c45bae9903341fc
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-06-16 21:42:48 +0200

    fix col_noe

--- packages/DT/datatable.R
+++ packages/DT/datatable.R
@@ -1,6 +1,6 @@
 df <- data.frame(
-   col_one = ( 5    , 3     , 1   , 4,   , 7,    , 9    , 6   ),
-   col_two = ('five','three','one','four','seven','nine','six')
+   col_one = c( 5    , 3     , 1   , 4    , 7     , 9    , 6   ),
+   col_two = c('five','three','one','four','seven','nine','six')
 );
 
 DT::datatable(df);
