896eefdd122fb190be63408ba7d0a2f54ad28eec
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-08-26 20:48:42 +0200

    dos -> unix

--- libraries/scatterplot3d/example-1.R
+++ libraries/scatterplot3d/example-1.R
@@ -1,21 +1,18 @@
 #
 #   ?scatterplot3d::scatterplot3d
 #   -> example 1
-#    
+#
 
 library(scatterplot3d)
 
-z <- seq(-10, 10, 0.01)
-x <- cos(z)
-y <- sin(z)
-
-X11()
+z <- seq(-10, 10, 0.01);
+x <- cos(z);
+y <- sin(z);
 
 scatterplot3d(x, y, z,
-  highlight.3d =  TRUE,
-  col.axis     = "blue",
-  col.grid     = "lightblue",
-  main         = "scatterplot3d - 1",
-  pch          =  20)
-
-z <- locator(1)
+   highlight.3d =  TRUE,
+   col.axis     = 'blue',
+   col.grid     = 'lightblue',
+   main         = 'scatterplot3d - 1',
+   pch          =  20
+);
