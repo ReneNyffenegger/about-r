580bc5873e8312a524e7e63a0016ea34ce1f10bb
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-08 13:27:03 +0100

    Update connect.SQLServer.R

--- packages/RODBC/connect.SQLServer.R
+++ packages/RODBC/connect.SQLServer.R
@@ -1,6 +1,6 @@
 library(RODBC);
 
-rodbc_con <- odbcDriverConnect('driver={SQL Server};server=OMIS-NC-08;database=tq84_db;trusted_connection=true')
+rodbc_con <- odbcDriverConnect('driver={SQL Server};server=TQ84-PC;database=tq84_db;trusted_connection=true')
 
 res <- sqlQuery(rodbc_con, 'select * from information_schema.tables');
 
